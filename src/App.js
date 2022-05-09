9d59d6aa81bb2f6b081bbd7fac6ebe1ac9f84bd7
cristian chaime <ectam484@gmail.com>
2022-05-09 19:07:11 -0300

    avance

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductCard from "../src/pages/ProductCard";
+import NewCategory from "./components/FormCategory";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<ProductCard />}></Route>
+          <Route path="/" element={<ProductCard />} />
           <Route path="/products/:id" />
+          <Route path="/formulario/" element={<NewCategory />} />
         </Routes>
       </Router>
     </>

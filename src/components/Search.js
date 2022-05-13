4edfa519f8dab12b578721f7ab335778205f1d6d
nacho-bootcamp <ignaciocardozoxd12@gmail.com>
2022-05-13 17:41:19 -0300

    nacho

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React ,{useState} from "react";
 import "./search.css";
 import { Input } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-const search = () => {
+import { useState } from "react";
+
+const searchBar = () =>{ 
+const [search , setSearch]= useState ("")
+ 
+//busqueda
+const searcher = (e)=>{
+  setSearch(e.target.value)
+}
+
+  //filtrado
+
+let resultado =[]
+if (!search) 
+{
+ resultado = product  //hay que borrar product y poner el el arreglo original de los productos 
+}else {
+  resultado = product.filter ((description) =>
+   description.name.toLowercase().includes(search.toLocaleLowerCase())
+   )
+}; 
+
+
   return (
     <div className="input">
-      <Input className="buscador" placeholder="Search" />
+      <Input className="buscador" 
+      value={search}
+      onChange={searcher}
+      type="text"
+      placeholder="Search" />
       <button className="btn">
         <FontAwesomeIcon icon={faSearch} />
       </button>
@@ -14,4 +40,4 @@ const search = () => {
   );
 };
 
-export default search;
+export default searchBar ;
